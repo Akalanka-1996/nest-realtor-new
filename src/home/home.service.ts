52df43e68a3a3736ae411442ba08e8bc4d9edd0d
“Kasun <kasunakalanka4@gmail.com>
2023-09-28 00:59:25 +0530

    Delete and Update by user

---
+++ src/home/home.service.ts
@@ -0,0 +1,184 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { HomeResponseDto } from './dto/home.dto';
+import { PropertyType } from '@prisma/client';
+
+interface GetHomesParam {
+  city?: string;
+  price?: {
+    gte?: number;
+    lte?: number;
+  };
+  propertyType?: PropertyType;
+}
+
+interface GetHomeByIdParam {
+  id: number;
+}
+
+interface CreateHomeParams {
+  address: string;
+  numberOfBedrooms: number;
+  numberOfBathrooms: number;
+  city: string;
+  price: number;
+  landSize: number;
+  propertyType: PropertyType;
+  images: { url: string }[];
+}
+
+interface UpdateHomeParams {
+  address?: string;
+  numberOfBedrooms?: number;
+  numberOfBathrooms?: number;
+  city?: string;
+  price?: number;
+  landSize?: number;
+  propertyType?: PropertyType;
+}
+
+@Injectable()
+export class HomeService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getHomes(filters: GetHomesParam): Promise<HomeResponseDto[]> {
+    const homes = await this.prismaService.home.findMany({
+      select: {
+        id: true,
+        address: true,
+        city: true,
+        price: true,
+        propertyType: true,
+        number_of_bathrooms: true,
+        number_of_bedrooms: true,
+        images: {
+          select: {
+            url: true,
+          },
+          take: 1, // take one image
+        },
+      },
+      where: filters,
+    });
+
+    if (!homes.length) {
+      throw new NotFoundException();
+    }
+    return homes.map((home) => {
+      const fetchedHome = { ...home, image: home.images[0].url };
+      delete fetchedHome.images;
+      return new HomeResponseDto(fetchedHome);
+    });
+  }
+
+  // async getHomeById(id: any): Promise<HomeResponseDto> {
+  //   const home = await this.prismaService.home.findUnique({
+  //     where: {
+  //       id,
+  //     },
+  //   });
+
+  //   if (!home) {
+  //     throw new NotFoundException();
+  //   }
+
+  //   console.log('home', home);
+
+  //   return home;
+  // }
+
+  async createHome(
+    {
+      address,
+      numberOfBedrooms,
+      numberOfBathrooms,
+      city,
+      price,
+      landSize,
+      propertyType,
+      images,
+    }: CreateHomeParams,
+    userId: number,
+  ) {
+    const home = await this.prismaService.home.create({
+      data: {
+        address,
+        number_of_bathrooms: numberOfBathrooms,
+        number_of_bedrooms: numberOfBedrooms,
+        city,
+        land_size: landSize,
+        propertyType,
+        price,
+        realtor_id: userId,
+      },
+    });
+
+    const homeImages = images.map((image) => {
+      return { ...image, home_id: home.id };
+    });
+
+    await this.prismaService.image.createMany({ data: homeImages });
+
+    return new HomeResponseDto(home);
+  }
+
+  async updateHomeById(id: number, data: UpdateHomeParams) {
+    const home = await this.prismaService.home.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!home) {
+      throw new NotFoundException();
+    }
+
+    const updatedHome = await this.prismaService.home.update({
+      where: {
+        id,
+      },
+      data,
+    });
+
+    return new HomeResponseDto(updatedHome);
+  }
+
+  async deleteHomeById(id: number) {
+    await this.prismaService.image.deleteMany({
+      where: {
+        home_id: id,
+      },
+    });
+
+    await this.prismaService.home.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async getRealtorByHomeId(id: number) {
+    console.log('comes here');
+    const home = await this.prismaService.home.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        realtor: {
+          select: {
+            name: true,
+            id: true,
+            email: true,
+            phone: true,
+          },
+        },
+      },
+    });
+
+    if (!home) {
+      throw new NotFoundException();
+    }
+    console.log('home', home);
+    return home.realtor;
+  }
+}
