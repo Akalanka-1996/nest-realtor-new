35f333639874e4bc26f8a3e40c84ee43729a5603
“Kasun <kasunakalanka4@gmail.com>
2023-09-28 10:26:01 +0530

    verify jwt

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,16 +1,30 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext) {
     // grab the roles from reflector
     const roles = this.reflector.getAllAndOverride('roles', [
       context.getHandler(),
       context.getClass(),
     ]);
+
+    if (roles.length) {
+      // grab the jwt from the request header and verify it
+      const request = context.switchToHttp().getRequest();
+      const token = request.headers?.authorization?.split('Bearer ')[1];
+      try {
+        const user = await jwt.verify(token, process.env.JWT_SECRET);
+        console.log('user', user);
+        return true;
+      } catch (error) {
+        return false;
+      }
+    }
     return true;
   }
 }
