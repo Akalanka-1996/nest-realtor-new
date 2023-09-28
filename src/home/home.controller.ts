926109ea73b3a9014b37a9d535c3ff1ffbf7acec
“Kasun <kasunakalanka4@gmail.com>
2023-09-28 11:18:02 +0530

    some refactoring

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -50,12 +50,12 @@ export class HomeController {
   // }
 
   @Roles(UserType.REALTOR)
-  @UseGuards(AuthGuard)
   @Post()
   createHome(@Body() body: CreateHomeDto, @User() user: UserInfo) {
     return this.homeService.createHome(body, user.id);
   }
 
+  @Roles(UserType.REALTOR)
   @Put(':id')
   async updateHome(
     @Param('id', ParseIntPipe) id: number,
@@ -71,6 +71,7 @@ export class HomeController {
     return this.homeService.updateHomeById(id, body);
   }
 
+  @Roles(UserType.REALTOR)
   @Delete(':id')
   async deleteHome(
     @Param('id', ParseIntPipe) id: number,
